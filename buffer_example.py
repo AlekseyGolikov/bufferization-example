52f4d3562a3393316a0e06e95422a3f99e1e0dbd
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-05-27 21:53:46 +0300

    decoration to instance function

--- buffer_example.py
+++ buffer_example.py
@@ -1,7 +1,5 @@
 import time
 
-MAXBUFFERSIZE = 10
-
 def time_decor(func):
     def wrapper(*args):
         start = time.perf_counter()
@@ -12,26 +10,33 @@ def time_decor(func):
             file.write('\n-----Функция отработала %6.5f----- \n' % runtime)
     return wrapper
 
-def count(n):
-    while n > 0:
-        yield "Очередное значение %d \n" % n
-        n -= 1
-    yield "Счет окончен \n"
 
-@time_decor
-def buffer():
-    chunks = ''
-    buffered_size = 0
-    for chunk in count(5):
-        for ch in chunk:
-            chunks += ''.join(ch)
-            buffered_size += 1
-            if buffered_size >= MAXBUFFERSIZE:
-                chunks += '...'
-                print(chunks)
-                with open('out.txt', 'a', encoding='utf-8') as f:
-                    f.write(chunks)
-                chunks = ''
-                buffered_size = 0
+class Buffer:
 
-buffer()
+    MAXBUFFERSIZE = 10
+
+    def count(self, n):
+        while n > 0:
+            yield "Очередное значение %d \n" % n
+            n -= 1
+        yield "Счет окончен \n"
+
+    @time_decor
+    def buffer(self, n):
+        chunks = ''
+        buffered_size = 0
+        for chunk in self.count(n):
+            for ch in chunk:
+                chunks += ''.join(ch)
+                buffered_size += 1
+                if buffered_size >= Buffer.MAXBUFFERSIZE:
+                    chunks += '...'
+                    print(chunks)
+                    with open('out.txt', 'a', encoding='utf-8') as f:
+                        f.write(chunks)
+                    chunks = ''
+                    buffered_size = 0
+
+n = 5
+b = Buffer()
+b.buffer(n)
