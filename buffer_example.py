05d1eec839e87a545e78858b071f0f2ca04010f1
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-05-27 21:37:12 +0300

    decoration of separated function

--- buffer_example.py
+++ buffer_example.py
@@ -1,4 +1,16 @@
+import time
+
 MAXBUFFERSIZE = 10
+
+def time_decor(func):
+    def wrapper(*args):
+        start = time.perf_counter()
+        func(*args)
+        runtime = time.perf_counter() - start
+        print('Функция отработала %6.5f' % runtime)
+        with open('out.txt', 'a', encoding='utf-8') as file:
+            file.write('\n-----Функция отработала %6.5f----- \n' % runtime)
+    return wrapper
 
 def count(n):
     while n > 0:
@@ -6,16 +18,20 @@ def count(n):
         n -= 1
     yield "Счет окончен \n"
 
-chunks = ''
-buffered_size = 0
-for chunk in count(5):
-    for ch in chunk:
-        chunks += ''.join(ch)
-        buffered_size += 1
-        if buffered_size >= MAXBUFFERSIZE:
-            chunks += '...'
-            print(chunks)
-            with open('out.txt', 'a', encoding='utf-8') as f:
-                f.write(chunks)
-            chunks = ''
-            buffered_size = 0
+@time_decor
+def buffer():
+    chunks = ''
+    buffered_size = 0
+    for chunk in count(5):
+        for ch in chunk:
+            chunks += ''.join(ch)
+            buffered_size += 1
+            if buffered_size >= MAXBUFFERSIZE:
+                chunks += '...'
+                print(chunks)
+                with open('out.txt', 'a', encoding='utf-8') as f:
+                    f.write(chunks)
+                chunks = ''
+                buffered_size = 0
+
+buffer()
